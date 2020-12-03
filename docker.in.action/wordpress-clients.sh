b3842a4f1665f9afcd9279446f49e269ced9158c
paul <pstivers3@comcast.net>
2020-12-02 23:17:38 -0800

    remove extra --read-only

--- docker.in.action/wordpress-clients.sh
+++ docker.in.action/wordpress-clients.sh
@@ -16,7 +16,7 @@ MAILER_CID=$(docker create dockerinaction/ch2_mailer)
 docker start $MAILER_CID
 
 WP_CID=$(docker create  --link $DB_CID:mysql --name wp_$CLIENT_ID -p 80 --read-only -v /run/apache2/ --tmpfs /tmp \
-    -e WORDPRESS_DB_NAME=$CLIENT_ID --read-only wordpress:5.0.0-php7.2-apache)
+    -e WORDPRESS_DB_NAME=$CLIENT_ID wordpress:5.0.0-php7.2-apache)
 docker start $WP_CID
 
 AGENT_CID=$(docker create --name agent_$CLIENT_ID --link $WP_CID:insideweb \
